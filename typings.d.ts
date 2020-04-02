8b4954ae3d65d25305d29c3e561c9e8128ea4ce8
siwei.tao <siwei.tao@zstack.io>
2020-04-02 16:51:38 +0800

    add object types

--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,19 @@
 declare module '*.css';
 declare module '*.less';
-declare module "*.png";
+declare module '*.png';
+
+declare interface PlainObject {
+  [propName: string]: any;
+}
+
+declare interface BooleanObject {
+  [propName: string]: boolean;
+}
+
+declare interface StringObject {
+  [propName: string]: string;
+}
+
+declare interface NumberObject {
+  [propName: string]: number;
+}
