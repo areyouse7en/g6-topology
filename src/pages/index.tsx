64e1d7571b7eeb835d54c0628412b4b452c3086f
siwei.tao <siwei.tao@zstack.io>
2020-04-02 17:27:38 +0800

    grid & state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import React, { SFC, useRef, useState, useEffect } from 'react';
 import { useRequest } from 'umi';
-import { TreeGraph } from '@antv/g6';
+import { TreeGraph, Grid } from '@antv/g6';
 import style from './style.less';
+import { IG6GraphEvent } from '@antv/g6/lib/types';
 
 const Index: SFC = () => {
-  const [graph, setGraph] = useState<TreeGraph>()
+  const [graph, setGraph] = useState<TreeGraph>();
   const ref = useRef<HTMLDivElement>(null);
   const { data, error, loading } = useRequest('/api/vm');
 
@@ -14,48 +15,71 @@ const Index: SFC = () => {
     if (!graph && container) {
       const width = container.scrollWidth;
       const height = container.scrollHeight;
+      const grid = new Grid();
       const _graph = new TreeGraph({
         container,
         width,
         height,
         modes: {
-          default: ["drag-canvas", "zoom-canvas"]
+          default: ['drag-canvas', 'zoom-canvas'],
         },
         defaultNode: {
-          type: "rect",
-          size: [200, 100],
-          anchorPoints: [[0.5, 0], [0.5, 1]],
+          type: 'rect',
           style: {
-            opacity: 0.3
-          }
+            cursor: 'pointer',
+          },
+          anchorPoints: [
+            [0.5, 0],
+            [0.5, 1],
+          ],
         },
         defaultEdge: {
-          type: "cubic-vertical",
+          type: 'cubic-vertical',
           style: {
-            stroke: "#A3B1BF"
-          }
+            stroke: '#A3B1BF',
+          },
+        },
+        nodeStateStyles: {
+          hover: {
+            shadowColor: 'rgba(0,84,147,0.1)',
+            shadowBlur: 10,
+          },
         },
         layout: {
-          type: "compactBox",
-          direction: "TB",
-          getVGap: function getVGap() {
+          type: 'compactBox',
+          direction: 'TB',
+          getVGap() {
             return 50;
           },
-          getHGap: function getHGap() {
+          getHGap() {
             return 100;
-          }
-        }
-      })
+          },
+        },
+        plugins: [grid],
+      });
+
+      // 启用状态
+      _graph.on('node:mouseenter', (e: IG6GraphEvent) => {
+        const { item } = e;
+        _graph.setItemState(item!, 'hover', true);
+      });
+
+      _graph.on('node:mouseleave', (e: IG6GraphEvent) => {
+        const { item } = e;
+        _graph.setItemState(item!, 'hover', false);
+      });
 
       _graph.data(data);
       _graph.render();
       _graph.fitView();
       setGraph(_graph);
     }
-  }
+  };
 
   useEffect(() => {
-    initGraph();
+    if (data) {
+      initGraph();
+    }
   }, [data]);
 
   if (loading) {
@@ -65,10 +89,7 @@ const Index: SFC = () => {
     return <div>{error.message}</div>;
   }
 
-  return (
-    <div ref={ref} className={style.container} />
-  );
-}
+  return <div ref={ref} className={style.container} />;
+};
 
 export default Index;
-
